0ac4c8a4db826fcba7b6c14e41bee22eff19e101
Dmytro <dimaniko04@ukr.net>
2020-08-31 22:06:39 +0300

    Header was added , first page was created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Header.scss";
+import { Link } from "react-router-dom";
+
+const CornerButton = (props) => (
+  <Link to={props.link} className={`cornerButton${props.type}`}>
+    <p>{props.content}</p>
+  </Link>
+);
+
+export const Header = (props) => {
+  const theme = props.backgroundTheme ? props.backgroundTheme : "none";
+  const logoTheme = props.logoTheme;
+  const buttonProps = props.buttonProps;
+  return (
+    <header className={`Header-${theme}`}>
+      <h1 className={`Logo-${logoTheme}`}>S2</h1>
+      <CornerButton
+        content={buttonProps.content}
+        link={buttonProps.link}
+        type={buttonProps.type}
+      />
+    </header>
+  );
+};
